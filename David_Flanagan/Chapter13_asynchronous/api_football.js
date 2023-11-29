eee9cef52c029f8bd9063acdc019921d69907326
Ð⨊⩔ ⨙ɤαηΚ <lucasfrank254@gmail.com>
2023-11-29 20:29:50 +0300

    using the fetch, async and await

---
+++ David_Flanagan/Chapter13_asynchronous/api_football.js
@@ -0,0 +1,40 @@
+// use the football api
+const fetchData = async () => {
+    const response = await fetch("./league-tables.json");
+    if (!response.ok)
+        throw new Error("Cannot get the file required.")
+    return await response.json()
+}
+
+const iterateData = async (iterable) => {
+    return new Promise((resolve, reject) => {
+        // iterate the objects
+        if (!iterable) {
+            reject("Please ensure the data is valid");
+        }
+        iterable[Symbol.iterator] = function () {
+            let [iterator, next] = [Object.keys(this), 0];
+            const SELF = this;
+            return {
+                [Symbol.iterator]() {return SELF},
+                next() {
+                    return next < iterator.length ? {value: `${iterator[next++]}` , done: false} : {done: true};
+                }
+            }
+        }
+         resolve([...iterable]);
+    })
+}
+
+const printData = async (data) => {
+    return await iterateData(data);
+}
+fetchData()
+    .then(data => {
+        console.info("1. Successful: ");
+        return printData(data["data"]);
+    }).then(data => {
+    console.info("2. Successful: ");
+    console.table(data);
+})
+    .catch(err => console.log("Rejected:", err));
--- David_Flanagan/Chapter13_asynchronous/async_await.js
+++ David_Flanagan/Chapter13_asynchronous/async_await.js
@@ -4,6 +4,35 @@ const getTodos = async () => {
         throw new Error("cannot fetch the data!")
     return await response.json();
 }
+/*
 getTodos()
     .then(data => console.log('resolved:', data))
-    .catch(err => console.log('rejected', err.message));
+    .catch(err => console.log('rejected', err.message));*/
+// Function that returns a Promise
+const performAsyncOperation = async () => {
+    return new Promise((resolve, reject) => {
+        // Simulating an asynchronous operation
+        setTimeout(() => {
+            const success = true; // Simulate a successful operation
+            if (success) {
+                resolve("Operation completed successfully");
+            } else {
+                reject("Operation failed");
+            }
+        }, 2000); // Simulating a delay of 2 seconds
+    });
+};
+
+// Using async/await to handle the Promise
+const executeAsync = async () => {
+    try {
+        const result = await performAsyncOperation();
+        console.info("Success:", result);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+
+// Call the async function
+executeAsync();
+
