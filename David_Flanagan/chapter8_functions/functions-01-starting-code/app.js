a3c470a457c42373efcf09e8bfd07e5b4a5effc7
Ð⨊⩔ ⨙ɤαηΚ <lucasfrank254@gmail.com>
2023-12-06 17:24:41 +0300

    More on functions

--- David_Flanagan/chapter8_functions/functions-01-starting-code/app.js
+++ David_Flanagan/chapter8_functions/functions-01-starting-code/app.js
@@ -4,6 +4,9 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_CWIN = 'COMPUTER WINS';
+const RESULT_PWIN = 'PLAYER WINS';
 let gameIsRunning = false;
 const getPlayerChoice = () => {
     const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
@@ -18,10 +21,22 @@ const getPlayerChoice = () => {
     }
 }
 
+const getComputerChoice = () => {
+    const randomVal = Math.floor(Math.random() * 100);
+    return randomVal < 34 ? ROCK : randomVal >= 34 && randomVal < 67 ? PAPER : SCISSORS;
+}
+
+const determineWinner = (cChoice, pChoice) => {
+    return cChoice === pChoice ? RESULT_DRAW : cChoice === ROCK && pChoice === PAPER || cChoice === PAPER && pChoice === SCISSORS ||
+        cChoice === SCISSORS && pChoice === ROCK ? RESULT_PWIN : RESULT_CWIN;
+}
+
 startGameBtn.addEventListener('click', () => {
     if(gameIsRunning) return;
     gameIsRunning = true;
     console.log("Game is starting...");
     const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const compSelection = getComputerChoice();
+    console.log(`Computer: ${compSelection}, Player: ${playerSelection}`);
+    console.log(determineWinner(compSelection, playerSelection));
 });
